568ff17067b2c0fe36ac4408003263f73302a668
Făgăteanu Dumitru-Eusebiu <fagateanueusebiu@gmail.com>
2026-04-16 08:16:02 +0300

    Modify robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,11 +3,9 @@ import { MetadataRoute } from 'next';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*', // Regula se aplică pentru toți roboții (Google, Bing, Yahoo etc.)
-      allow: '/',     // Le permiți accesul pe întregul site
-      // disallow: '/admin/', -> Aici ai pune paginile pe care vrei să le ascunzi
+      userAgent: '*',
+      allow: '/',
     },
-    // Când vei urca site-ul online și vei avea un domeniu, e bine să pui calea către sitemap aici:
-    // sitemap: 'https://www.traduceriurgenteauto.ro/sitemap.xml',
+    sitemap: 'https://www.traduceriurgenteauto.ro/sitemap.xml',
   };
 }
